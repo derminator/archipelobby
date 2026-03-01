ee69be71cbd150d594eff6e628f654c67295f504
Michael <michaelzd95@gmail.com>
2026-03-01 17:34:58 -0500

    Remove plugin management block from `settings.gradle.kts` to simplify build configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,1 @@
-pluginManagement {
-    repositories {
-        maven { url = uri("https://repo.maven.apache.org/maven2") }
-        maven { url = uri("https://repo.spring.io/milestone") }
-        gradlePluginPortal()
-    }
-}
-
 rootProject.name = "archipelobby"
